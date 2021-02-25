84ec7266c5f5e345fb6c850f2c2457e687f3dea0
Edward (Henry's) <wwlui@bal31.cs.hku.hk>
2021-02-25 18:21:52 +0800

    first

---
+++ Snakefile
@@ -0,0 +1,75 @@
+import os
+configfile: "config.yaml"
+
+rule all:
+    input:
+        expand("{out_dir}/Clair_ensemble_out",out_dir=config["out_dir"])
+
+rule basecalling:
+    input: 
+        "fast5"
+    output:
+        touch(".basecalling.done")
+    shell:
+        "bin/ont-guppy/bin/guppy_basecaller -r -i {input} -s {config[out_dir]}/Guppy_out -c dna_r9.4.1_450bps_hac.cfg --device cuda:{config[gpu_device]} -q 0 --hp_correct 1 --qscore_filtering --min_qscore 3"
+
+rule analysis:
+    input:
+        ".basecalling.done"
+    output:
+        expand("{out_dir}/Guppy_out/all.pass.fq",out_dir=config["out_dir"])
+    shell:
+        "cat {config[out_dir]}/Guppy_out/sequencing_summary.txt | cut -f 15 | "
+        """awk '{{NR>2; sum += $1; n++ }} END {{ if (n > 0) print "[INFO] *average read quality: " sum / n; }}';"""
+
+        """echo "[INFO] *total throughput: " $(cat {config[out_dir]}/Guppy_out/pass/*.fastq | tee {output} | paste - - - - | cut -f 2 | tr -d '\n' | wc -c);"""
+        """echo "[INFO] *average read length: " $(cat {output} | awk '{{if(NR%4==2) {{count++; bases += length}} }} END{{print bases/count}}')"""
+
+if config["minionqc"]==True:
+    rule minionqc:
+        input:
+            expand("{out_dir}/Guppy_out/sequencing_summary.txt",out_dir=config["out_dir"])
+        output:
+            expand("{out_dir}/MinIONQC_out",out_dir=config["out_dir"])
+        run:
+        #if config["minionqc"]==True:
+            shell("MinIONQC.R -i {input} -o {output} -p {config[threads]}")
+
+rule porechop:
+    input:
+        expand("{out_dir}/Guppy_out/all.pass.fq",out_dir=config["out_dir"])
+    output:
+        expand("{out_dir}/Porechop_out/guppy_pass.porechop.fastq",out_dir=config["out_dir"])
+    run:
+        if config["discard_middle"]==True:
+            discard_middle_flag="--discard_middle"
+        else:
+            discard_middle_flag=""   
+        shell("mkdir -p {config[out_dir]}/Porechop_out && " \
+        "porechop -i {input} -o {output} -t {config[threads]} {discard_middle_flag}")
+
+rule align_index:
+    input:
+        expand("{out_dir}/Porechop_out/guppy_pass.porechop.fastq",out_dir=config["out_dir"])
+    output:
+        bam=expand("{out_dir}/Minimap2_out/guppy_pass.porechop.minimap2_hg38.sorted.bam",out_dir=config["out_dir"]),
+        bai=expand("{out_dir}/Minimap2_out/guppy_pass.porechop.minimap2_hg38.sorted.bam.bai",out_dir=config["out_dir"])
+    shell:
+        "mkdir -p {config[out_dir]}/Minimap2_out && "
+        "minimap2 -ax map-ont reference/GRCh38_no_alt_analysis_set.no_chr.fasta {input} -t {config[threads]} | samtools sort -o {output.bam}; "
+        "samtools index {output.bam}"
+
+rule clair_ensemble:
+    input:
+        bam=expand("{out_dir}/Minimap2_out/guppy_pass.porechop.minimap2_hg38.sorted.bam",out_dir=config["out_dir"]),
+        bai=expand("{out_dir}/Minimap2_out/guppy_pass.porechop.minimap2_hg38.sorted.bam.bai",out_dir=config["out_dir"])
+    output:
+        expand("{out_dir}/Clair_ensemble_out",out_dir=config["out_dir"])
+    run:
+        config["CLAIR_MODELS"]= [''.join([os.getcwd(),'/',path]) for path in config["CLAIR_MODELS"]]
+        clair_models_delim=','.join(config["CLAIR_MODELS"])
+        shell("./run_Clair_ensemble.sh -b {input.bam} -d {config[BED_FILE_PATH]} -r {config[REFERENCE_FASTA_FILE_PATH]} -c {config[CLAIR]} -m {clair_models_delim} -e {config[ENSEMBLE_CPP_EXECUTABLE]} -t {config[threads]} -o {output}")
+
+
+
+
---
+++ run_Clair_ensemble.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+while getopts b:d:r:c:m:e:t:o: option
+do
+    case "${option}"
+        in
+        # assumed given bam file is sorted and indexed
+        b) BAM_FILE_PATH=`realpath ${OPTARG}`;;
+        d) BED_FILE_PATH=`realpath ${OPTARG}`;;
+        r) REFERENCE_FASTA_FILE_PATH=`realpath ${OPTARG}`;;
+        c) CLAIR=`realpath ${OPTARG}`;;
+        m) old_IFS=$IFS
+           echo $IFS
+           IFS=','
+           CLAIR_MODELS=($OPTARG)
+           IFS=${old_IFS};;
+        e) ENSEMBLE_CPP_EXECUTABLE=`realpath ${OPTARG}`;;
+        t) PARALLEL_THREADS=${OPTARG};;
+        o) ROOT_FOLDER_PATH=`realpath ${OPTARG}`;;
+    esac
+done
+
+DIR_PATH=`pwd`
+NO_OF_CLAIR_MODELS=${#CLAIR_MODELS[@]}
+DATE_TIME=`date "+%Y%m%d_%H%M%S"`
+WORKING_DIRECTORY="${ROOT_FOLDER_PATH}/${DATE_TIME}"
+mkdir -p ${WORKING_DIRECTORY}
+BAM_FILE_PATHS=("${BAM_FILE_PATH}")
+INTERMEDIATE_OUTPUT_FOLDER="${WORKING_DIRECTORY}/tmp_output"
+mkdir  ${INTERMEDIATE_OUTPUT_FOLDER}
+cd ${INTERMEDIATE_OUTPUT_FOLDER}
+for i in "${!BAM_FILE_PATHS[@]}"
+do
+  INPUT_BAM_FILE_PATH="${BAM_FILE_PATHS[i]}"
+  SAMPLE_NAME="MES"
+  BAM_PREFIX=`printf "%02d" ${i}`
+
+  for j in "${!CLAIR_MODELS[@]}"
+  do
+    CLAIR_MODEL="${CLAIR_MODELS[j]}"
+    MODEL_PREFIX=`printf "%02d" ${j}`
+    SCRIPT_OUTPUT_FOLDER="m${MODEL_PREFIX}_b${BAM_PREFIX}"
+
+    mkdir ${SCRIPT_OUTPUT_FOLDER}
+    OUTPUT_PREFIX="${SCRIPT_OUTPUT_FOLDER}/tmp"
+
+    python ${CLAIR} callVarBamParallel \
+    --chkpnt_fn "${CLAIR_MODEL}" \
+    --ref_fn "${REFERENCE_FASTA_FILE_PATH}" \
+    --bed_fn "${BED_FILE_PATH}" \
+    --bam_fn "${INPUT_BAM_FILE_PATH}" \
+    --pysam_for_all_indel_bases \
+    --output_for_ensemble \
+    --refChunkSize "10000000" \
+    --sampleName "${SAMPLE_NAME}" \
+    --output_prefix "${OUTPUT_PREFIX}" > ${SCRIPT_OUTPUT_FOLDER}/call.sh
+  done
+done
+( time cat */call.sh | parallel -j${PARALLEL_THREADS} ) |& tee ${INTERMEDIATE_OUTPUT_FOLDER}/log.call.txt
+
+FILES=(`ls m00_b00/*.vcf`)
+ENSEMBLE_OUTPUT_FOLDER="${INTERMEDIATE_OUTPUT_FOLDER}/ensemble"
+mkdir ${ENSEMBLE_OUTPUT_FOLDER}
+MININUM_NO_OF_VOTE_FOR_VARIANT="$(((${#BAM_FILE_PATHS[@]}*${#CLAIR_MODELS[@]}+2)/2))"
+rm ensemble_command.sh
+for i in "${!FILES[@]}"
+do
+  TARGET_FILE_NAME="${FILES[i]:8}"
+  CAT_COMMAND=""
+  for j in "${!BAM_FILE_PATHS[@]}"
+  do
+    BAM_PREFIX=`printf "%02d" ${j}`
+    for k in "${!CLAIR_MODELS[@]}"
+    do
+      MODEL_PREFIX=`printf "%02d" ${k}`
+      FOLDER_NAME="m${MODEL_PREFIX}_b${BAM_PREFIX}"
+      CAT_COMMAND="${CAT_COMMAND} ${FOLDER_NAME}/${TARGET_FILE_NAME}"
+    done
+  done
+
+
+  echo "cat ${CAT_COMMAND:1} | ${ENSEMBLE_CPP_EXECUTABLE} ${MININUM_NO_OF_VOTE_FOR_VARIANT} > ${ENSEMBLE_OUTPUT_FOLDER}/${TARGET_FILE_NAME}" >> ensemble_command.sh
+done
+( time cat ensemble_command.sh | parallel -j${PARALLEL_THREADS} ) |& tee ${INTERMEDIATE_OUTPUT_FOLDER}/log.ensemble.txt
+
+
+VCF_OUTPUT_FOLDER="${WORKING_DIRECTORY}/output"
+mkdir ${VCF_OUTPUT_FOLDER}
+cd ${WORKING_DIRECTORY}
+INPUT_FILES=(`ls tmp_output/ensemble/*.vcf`)
+
+for i in "${!INPUT_FILES[@]}"
+do
+  FILE_NAME="${INPUT_FILES[i]:20}"
+  echo "cat tmp_output/ensemble/${FILE_NAME} | \
+  python ${CLAIR} call_var \
+  --chkpnt_fn "${CLAIR_MODEL}" \
+  --ref_fn "${REFERENCE_FASTA_FILE_PATH}" \
+  --bam_fn "${BAM_FILE_PATH}" \
+  --call_fn "${VCF_OUTPUT_FOLDER}/${FILE_NAME}" \
+  --sampleName "MES" \
+  --pysam_for_all_indel_bases \
+  --input_probabilities" >> output.sh
+
+done
+( time cat output.sh | parallel -j${PARALLEL_THREADS} ) |& tee ${INTERMEDIATE_OUTPUT_FOLDER}/log.output.txt
+
+
+cd ${WORKING_DIRECTORY}
+vcfcat ${VCF_OUTPUT_FOLDER}/*.vcf | sort -k1,1V -k2,2n > snp_and_indel.vcf
+cat snp_and_indel.vcf | python ${DIR_PATH}/bin/Clair-ensemble/Clair.beta.ensemble.cpu/clair/post_processing/overlap_variant.py > snp_and_indel.filtered.vcf
+
+
+
